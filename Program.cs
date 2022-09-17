13234f3990482f3ca64aa17cf805b809a9e23435
Graceless <syerdnaswag@gmail.com>
2022-09-18 01:27:43 +0300

    Added tasks

--- Program.cs
+++ Program.cs
@@ -1,1 +1,227 @@
-﻿
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, 
+// которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int[,] GetArray(int m, int n)
+{
+    int[,] arr = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            arr[i, j] = new Random().Next(10);
+        }
+    }
+    return arr;
+}
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            System.Console.Write($"{arr[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[,] res = GetArray(4, 5);
+PrintArray(res);
+
+System.Console.WriteLine();
+
+int[,] SetArrayIncrease(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = j + 1; k < array.GetLength(1); k++)
+            {
+                if (array[i, j] < array[i, k])
+                {
+                    int temp = array[i, j];
+                    array[i, j] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+    return array;
+}
+
+int[,] res2 = SetArrayIncrease(res);
+PrintArray(res2);
+
+// Задача 56: Задайте прямоугольный двумерный массив. 
+// Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+// Например, задан массив:
+
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+int[,] res3 = GetArray(5, 5);
+PrintArray(res3);
+
+System.Console.WriteLine();
+
+void ShowStrokeMinSum(int[,] array)
+{
+    int[] strokeSums = new int[array.GetLength(0)];
+    int minSum = strokeSums[0];
+    int index = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            strokeSums[i] += array[i, j];
+        }
+    }
+    int min = 0;
+    for (int k = 1; k < strokeSums.Length; k++)
+    {
+        if (strokeSums[k] < strokeSums[min])
+        {
+            min = k;
+        }
+    }
+    System.Console.WriteLine($"Номер строки с минимальной суммой элементов: {min}");
+    System.Console.WriteLine();
+    System.Console.WriteLine("Суммы строк:");
+    foreach (var item in strokeSums)
+    {
+        System.Console.WriteLine(item);
+    }
+}
+
+ShowStrokeMinSum(res3);
+
+
+// Задача 58: Задайте две матрицы. Напишите программу, 
+// которая будет находить произведение двух матриц.
+
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+int[,] MultiMatrix(int[,] arr1, int[,] arr2)
+{
+    int[,] result = new int[arr1.GetLength(0), arr2.GetLength(1)];
+    for (int i = 0; i < arr1.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr1.GetLength(0); j++)
+        {
+            for (int k = 0; k < arr2.GetLength(1); k++)
+            {
+                result[i, k] += arr1[i, j] * arr2[j, k];
+            }
+        }
+    }
+    return result;
+}
+
+int[,] arr1 = { { 2, 4 }, { 3, 2 } };
+int[,] arr2 = { { 3, 4 }, { 3, 3 } };
+
+int[,] res = MultiMatrix(arr1, arr2);
+PrintArray(res);
+
+
+// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+int[,,] Get3DArray(int a, int b, int c)
+{
+    int[,,] result = new int[a, b, c];
+    Random rnd = new Random();
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            for (int k = 0; k < result.GetLength(2); k++)
+            {
+                result[i, j, k] = rnd.Next(100);
+            }
+        }
+    }
+    return result;
+}
+
+void Print3DArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                System.Console.WriteLine($"{(array[i, j, k])}({i},{j},{k})");
+            }
+        }
+    }
+}
+
+int[,,] res1 = Get3DArray(2, 2, 2);
+Print3DArray(res1);
+
+
+// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+
+int[,] GetSpiral(int n)
+{
+    int[,] result = new int[n, n];
+    int pos = 1;
+    int count = n;
+    int value = -n;
+    int sum = -1;
+
+    do
+    {
+        value = -1 * value / n;
+        for (int i = 0; i < count; i++)
+        {
+            sum += value;
+            result[sum / n, sum % n] = pos++;
+        }
+        value *= n;
+        count--;
+        for (int i = 0; i < count; i++)
+        {
+            sum += value;
+            result[sum / n, sum % n] = pos++;
+        }
+    } while (count > 0);
+
+    return result;
+}
+
+
+int[,] result = GetSpiral(4);
+PrintArray(result);
